2a2e1973774ea35a8fe24aa95ea6fb1b7825536a
Natasha Laurie <natusik711@gmail.com>
2022-04-09 18:46:24 +0300

    feat: add response handling for searching news

--- app/src/main/java/com/example/topnews/repository/NewsRepository.kt
+++ app/src/main/java/com/example/topnews/repository/NewsRepository.kt
@@ -9,4 +9,7 @@ class NewsRepository(
     suspend fun getTopNews(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getTopNews(countryCode, pageNumber)
 
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
 }
